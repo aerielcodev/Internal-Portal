634fdd5b2bf82b514842c381090d845737450ee5
sparkle-mermaid <sparkle.mermaid@live.com>
2022-09-09 09:23:51 -0600

    commit

---
+++ Customer Users.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT
+      u.Id,
+      u.UserId,
+      u.Email,
+      u.FirstName + ' ' + u.LastName AS Name,
+      c.CompanyName AS Customer,
+      cr.Name AS 'Role Name',
+      u.[Status],
+      u.IsActive
+  FROM CustomerUsers u
+  LEFT JOIN Customers c ON c.Id = u.CustomerId
+  LEFT JOIN CustomerUserRoles cr ON cr.Id = u.CustomerUserRoleId
+  WHERE (u.FirstName + ' ' + u.LastName IS NOT NULL) AND (u.FirstName + ' ' + u.LastName != ' ')
