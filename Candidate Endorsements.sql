ee4170f8edbe5c4da464236a9826d09e092e9c8e
sparkle-mermaid <sparkle.mermaid@live.com>
2022-12-07 13:41:06 -0700

    added who endorsed the candidate and if a customer has requested an interview

--- Candidate Endorsements.sql
+++ Candidate Endorsements.sql
@@ -12,11 +12,15 @@ SELECT
         WHEN jc.Recommendation = 1 THEN 'Endorse'
         WHEN jc.Recommendation = 2 THEN 'Do Not Endorse'
         WHEN jc.Recommendation = 3 THEN 'Not A Fit'
-    END AS Recommendation,
+    END AS Recommendation,    
+    en.FirstName + ' ' + en.LastName AS 'Endorsed By',
     jc.EndorsementStatusChangeDate AS 'Endorsement Status Change Date',
+    IIF(jc.InterviewRequestedBy IS NOT NULL,'Y','N') AS 'Interview Requested',
     jc.JobOpeningId AS JobOpeningId,
     jc.InterviewRequestedBy AS 'Interview Requested By'
 FROM JobOpeningCandidates jc
 LEFT JOIN CandidateProfileInformations c ON c.Id = jc.CandidateId 
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) e ON e.Id = jc.EmployeeId
 LEFT JOIN JobOpeningEndorsementStatuses js ON js.Id = jc.JobOpeningEndorsementStatusId
+LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) en ON en.UserId = jc.CreatedBy
+
