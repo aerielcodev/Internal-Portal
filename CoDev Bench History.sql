81c42fcaaff46626cc7cda5dd323e3355695a989
sparkle-mermaid <sparkle.mermaid@live.com>
2023-01-05 11:55:31 -0700

    included the columns that shows who created and modified the placment

--- CoDev Bench History.sql
+++ CoDev Bench History.sql
@@ -4,9 +4,13 @@ SELECT
     upper(emp.FirstName + ' ' + emp.LastName) AS 'Team Member',
     c.CompanyName,
     ce.DateStart,
-    ce.DateEnd
+    ce.DateEnd,
+    cb.FirstName + ' ' + cb.LastName,
+    m.FirstName + ' ' + m.LastName
 FROM Customers c 
 LEFT JOIN CustomerEmployees ce ON ce.CustomerId = c.Id
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
-WHERE c.Id = 1
+LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) m ON m.UserId = ce.LastModifiedBy
+LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) cb ON c.UserId = ce.LastModifiedBy
+WHERE c.Id = 1 
 ORDER BY [Team Member] ASC
