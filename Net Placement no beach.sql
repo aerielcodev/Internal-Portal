c1d0a052f47016a3641a81b9b0a9e1bdfab53cab
sparkle-mermaid <sparkle.mermaid@live.com>
2023-01-05 11:57:47 -0700

    added Modified By

---
+++ Net Placement no beach.sql
@@ -0,0 +1,33 @@
+SELECT
+
+    CONVERT(date,ce.DateStart) AS 'Placement/Offboard',
+    ce.id,
+    emp.CoDevId,
+    upper(trim(emp.FirstName) + ' ' + trim(emp.LastName)) AS 'Team Member',
+    c.CompanyName,
+    CONVERT(date,ce.DateStart) AS Placement,
+    CONVERT(date,ce.DateEnd) AS 'Placement End',
+    'Placement' AS 'Group',
+    m.FirstName + ' ' + m.LastName
+
+FROM Customers c 
+LEFT JOIN CustomerEmployees ce ON ce.CustomerId = c.Id
+LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
+LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) m ON m.UserId = ce.LastModifiedBy
+WHERE c.Id != 1 AND ce.Id IS NOT NULL AND c.CompanyName NOT LIKE 'codev%' AND c.CompanyName NOT LIKE '%breakthrough%' 
+UNION ALL
+SELECT
+    CONVERT(date,ce.DateEnd),
+    ce.id,
+    emp.CoDevId,
+    upper(trim(emp.FirstName) + ' ' + trim(emp.LastName)),
+    c.CompanyName,
+    CONVERT(date,ce.DateStart) ,
+    CONVERT(date,ce.DateEnd) ,
+    'Offboard' AS 'Group',
+    m.FirstName + ' ' + m.LastName
+FROM Customers c 
+LEFT JOIN CustomerEmployees ce ON ce.CustomerId = c.Id
+LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
+LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) m ON m.UserId = ce.LastModifiedBy
+WHERE c.Id != 1 AND ce.Id IS NOT NULL AND c.CompanyName NOT LIKE 'codev%' AND c.CompanyName NOT LIKE '%breakthrough%'  AND ce.DateEnd IS NOT NULL
