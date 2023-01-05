0f24f7cb458f47699052a259b58e32c18a27b52d
sparkle-mermaid <sparkle.mermaid@live.com>
2023-01-05 07:23:00 -0700

    created placement history without beach

---
+++ Placement History No Beach.sql
@@ -0,0 +1,12 @@
+SELECT
+    ce.id,
+    emp.CoDevId,
+    upper(trim(emp.FirstName) + ' ' + trim(emp.LastName)) AS 'Team Member',
+    c.CompanyName,
+    CONVERT(date,ce.DateStart) AS Placement,
+    CONVERT(date,ce.DateEnd) AS 'Placement End'
+FROM Customers c 
+LEFT JOIN CustomerEmployees ce ON ce.CustomerId = c.Id
+LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
+WHERE c.Id != 1 AND ce.Id IS NOT NULL AND c.CompanyName NOT LIKE 'codev%' AND c.CompanyName NOT LIKE '%breakthrough%' 
+ORDER BY [Team Member] ASC
