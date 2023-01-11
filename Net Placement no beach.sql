ec2dc12757b0d59ed4910c0bb0b4b4289b9c87ea
sparkle-mermaid <sparkle.mermaid@live.com>
2023-01-11 15:15:40 -0700

    added another column to identify who created the history

--- Net Placement no beach.sql
+++ Net Placement no beach.sql
@@ -8,13 +8,15 @@ SELECT
     CONVERT(date,ce.DateStart) AS Placement,
     CONVERT(date,ce.DateEnd) AS 'Placement End',
     'Placement' AS 'Group',
-    m.FirstName + ' ' + m.LastName
+    cr.FirstName + ' ' + cr.LastName AS 'Created By',
+    m.FirstName + ' ' + m.LastName AS 'Modified By'
 
 FROM Customers c 
 LEFT JOIN CustomerEmployees ce ON ce.CustomerId = c.Id
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
+LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) cr ON cr.UserId = ce.CreatedBy
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) m ON m.UserId = ce.LastModifiedBy
-WHERE c.Id != 1 AND ce.Id IS NOT NULL AND c.CompanyName NOT LIKE 'codev%' AND c.CompanyName NOT LIKE '%breakthrough%' 
+WHERE c.Id != 1 AND ce.Id IS NOT NULL AND (c.CompanyName NOT LIKE 'codev%' AND c.CompanyName NOT LIKE '%breakthrough%' )
 UNION ALL
 SELECT
     CONVERT(date,ce.DateEnd),
@@ -25,9 +27,11 @@ SELECT
     CONVERT(date,ce.DateStart) ,
     CONVERT(date,ce.DateEnd) ,
     'Offboard' AS 'Group',
+    cr.FirstName + ' ' + cr.LastName,
     m.FirstName + ' ' + m.LastName
 FROM Customers c 
 LEFT JOIN CustomerEmployees ce ON ce.CustomerId = c.Id
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
+LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) cr ON cr.UserId = ce.CreatedBy
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) m ON m.UserId = ce.LastModifiedBy
-WHERE c.Id != 1 AND ce.Id IS NOT NULL AND c.CompanyName NOT LIKE 'codev%' AND c.CompanyName NOT LIKE '%breakthrough%'  AND ce.DateEnd IS NOT NULL
+WHERE c.Id != 1 AND ce.Id IS NOT NULL AND (c.CompanyName NOT LIKE 'codev%' AND c.CompanyName NOT LIKE '%breakthrough%')  AND ce.DateEnd IS NOT NULL
