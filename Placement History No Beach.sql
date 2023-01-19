2de4c44ab248f0b8cca11b189cfbe0e2759857e7
sparkle-mermaid <sparkle.mermaid@live.com>
2023-01-19 11:00:56 -0700

    added offboarding reasons

--- Placement History No Beach.sql
+++ Placement History No Beach.sql
@@ -5,9 +5,20 @@ SELECT
     ce.JobTitle AS 'Job Title',
     c.CompanyName,
     ce.DateStart,
-    ce.DateEnd
+    ce.DateEnd,
+    oc.Name AS 'Offboarding Category',
+    r.subCat AS 'Offboarding SubCategory',
+    o.Note AS 'Offboarding Note'
 FROM Customers c 
 LEFT JOIN CustomerEmployees ce ON ce.CustomerId = c.Id
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
-WHERE c.Id != 1 AND ce.Id IS NOT NULL AND c.CompanyName NOT LIKE 'codev%' AND c.CompanyName NOT LIKE '%breakthrough%' 
-ORDER BY [Team Member] ASC
+LEFT JOIN EmployeeOffboardings o ON o.CustomerEmployeeId = ce.Id
+LEFT JOIN (
+    SELECT 
+    s2.OffboardingCategoryId,
+    s.EmployeeOffboardingId, 
+    STRING_AGG(s2.Name,';') AS subCat 
+FROM EmployeeOffboardingSubCategories s 
+INNER JOIN OffboardingSubCategories s2 ON s.OffboardingSubCategoryId = s2.Id GROUP BY s.EmployeeOffboardingId,s2.OffboardingCategoryId)  AS r ON r.EmployeeOffboardingId = o.Id
+LEFT JOIN OffboardingCategories oc ON oc.Id = r.OffboardingCategoryId
+WHERE c.Id != 1 AND ce.Id IS NOT NULL AND c.CompanyName NOT LIKE 'codev%' AND c.CompanyName NOT LIKE '%breakthrough%'
