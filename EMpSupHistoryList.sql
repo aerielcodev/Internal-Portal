db3c723accadbd5e6f437261ad9ef9b7ca440841
sparkle-mermaid <sparkle.mermaid@live.com>
2023-02-08 08:10:47 -0700

    added the customerEmployeesId

--- EMpSupHistoryList.sql
+++ EMpSupHistoryList.sql
@@ -4,8 +4,10 @@ SELECT
     sup.CodevId AS 'Supervisor CoDev Emp Num',
     emp.FirstName + ' ' + emp.LastName AS 'Team Member',
     sup.FirstName + ' ' + sup.LastName AS Supervisor,
-    es.EffectiveDate AS EffectiveDate
+    es.EffectiveDate AS EffectiveDate,
+    ce.Id AS CustomerEmployeeId
 FROM dbo.EmployeeSupervisors es
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = es.EmployeeId
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) sup ON sup.Id = es.SupervisorId
-LEFT JOIN dbo.CustomerEmployees ce ON ce.Id = es.CustomerEmployeeId
+INNER JOIN dbo.CustomerEmployees ce ON ce.Id = es.CustomerEmployeeId
+ORDER BY es.EffectiveDate DESC
