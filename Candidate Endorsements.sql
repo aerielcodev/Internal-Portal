999dfc679eddb1231ea4feaa73dfde12bac1d980
sparkle-mermaid <sparkle.mermaid@live.com>
2023-02-10 14:10:25 -0700

    added modified date

--- Candidate Endorsements.sql
+++ Candidate Endorsements.sql
@@ -24,6 +24,7 @@ SELECT
     jc.DateJobOfferAccepted,
     jc.DateOfOnboarding,
     jc.JobOpeningId AS JobOpeningId,
+    jc.LastModified,
     jc.InterviewRequestedBy AS 'Interview Requested By'
 FROM JobOpeningCandidates jc
 LEFT JOIN CandidateProfileInformations c ON c.Id = jc.CandidateId 
