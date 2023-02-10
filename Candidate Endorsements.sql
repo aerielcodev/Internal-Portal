05a1b7206fdec1622f135738f31862d5eea291ae
sparkle-mermaid <sparkle.mermaid@live.com>
2023-02-10 13:56:06 -0700

    added offer dates

--- Candidate Endorsements.sql
+++ Candidate Endorsements.sql
@@ -17,15 +17,20 @@ SELECT
     jc.Note,
     en.FirstName + ' ' + en.LastName AS 'Endorsed By',
     jc.EndorsementStatusChangeDate AS 'Endorsement Status Change Date',
+    d.FirstName + ' ' + d.LastName AS 'Declined By',
     IIF(jc.InterviewRequestedBy IS NOT NULL,'Y','N') AS 'Interview Requested',
+    jc.InitialInterviewRequestDate,
+    jc.JobOfferExtended,
+    jc.DateJobOfferAccepted,
+    jc.DateOfOnboarding,
     jc.JobOpeningId AS JobOpeningId,
-    jc.InterviewRequestedBy AS 'Interview Requested By',
-    jc.InitialInterviewRequestDate 
+    jc.InterviewRequestedBy AS 'Interview Requested By'
 FROM JobOpeningCandidates jc
 LEFT JOIN CandidateProfileInformations c ON c.Id = jc.CandidateId 
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) e ON e.Id = jc.EmployeeId
 LEFT JOIN JobOpeningEndorsementStatuses js ON js.Id = jc.JobOpeningEndorsementStatusId
 LEFT JOIN NotAFitCategoryTypes naft ON naft.Id = jc.NotAFitCategoryTypeId
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) en ON en.UserId = jc.CreatedBy /*user who endorsed the candidate*/
+LEFT JOIN CustomerUsers d ON d.UserId = jc.DeclinedBy
 WHERE jc.Created >= CONVERT(DATE,'2022-12-06') /*Dec. 6, 2022 is the official day that we started endorsing candidates to customers*/
 
