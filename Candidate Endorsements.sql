47915129a8b4e5bb4a2586f821a6e10c6ecba52d
sparkle-mermaid <sparkle.mermaid@live.com>
2023-02-10 13:44:50 -0700

    added Not A Fit Category and Notes

--- Candidate Endorsements.sql
+++ Candidate Endorsements.sql
@@ -13,6 +13,8 @@ SELECT
         WHEN jc.Recommendation = 2 THEN 'Do Not Endorse'
         WHEN jc.Recommendation = 3 THEN 'Not A Fit'
     END AS Recommendation,    
+    naft.Name AS 'Not A Fit Category',
+    jc.Note,
     en.FirstName + ' ' + en.LastName AS 'Endorsed By',
     jc.EndorsementStatusChangeDate AS 'Endorsement Status Change Date',
     IIF(jc.InterviewRequestedBy IS NOT NULL,'Y','N') AS 'Interview Requested',
@@ -23,6 +25,7 @@ FROM JobOpeningCandidates jc
 LEFT JOIN CandidateProfileInformations c ON c.Id = jc.CandidateId 
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) e ON e.Id = jc.EmployeeId
 LEFT JOIN JobOpeningEndorsementStatuses js ON js.Id = jc.JobOpeningEndorsementStatusId
+LEFT JOIN NotAFitCategoryTypes naft ON naft.Id = jc.NotAFitCategoryTypeId
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) en ON en.UserId = jc.CreatedBy /*user who endorsed the candidate*/
 WHERE jc.Created >= CONVERT(DATE,'2022-12-06') /*Dec. 6, 2022 is the official day that we started endorsing candidates to customers*/
 
