3dfff47799d5d56f1eb69ad55bfe9b555ef4b878
sparkle-mermaid <sparkle.mermaid@live.com>
2023-02-22 11:22:39 -0700

    added first rate for placement and last rate for offboarding

--- Net Placement no beach.sql
+++ Net Placement no beach.sql
@@ -7,6 +7,7 @@ SELECT
     jt.Name AS 'Job Opening Type',
     c.CompanyName,
     ce.DateStart AS 'Start',
+    fRate.NewRate AS 'Rate',
     ce.DateEnd AS 'End',
     'Placement' AS 'Group',
     cr.FirstName + ' ' + cr.LastName AS 'Created By',
@@ -20,6 +21,9 @@ LEFT JOIN Customers c ON ce.CustomerId = c.Id
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) cr ON cr.UserId = ce.CreatedBy
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) m ON m.UserId = ce.LastModifiedBy
+OUTER APPLY (
+    SELECT TOP 1 * FROM RateIncreases WHERE EmployeeId = ce.EmployeeId ORDER BY EffectiveDate ASC
+    ) AS fRate
 WHERE c.Id != 1 AND ce.Id IS NOT NULL AND (c.CompanyName NOT LIKE 'codev%' AND c.CompanyName NOT LIKE '%breakthrough%' ) AND c.Id != 281
 UNION ALL
 SELECT
@@ -31,6 +35,7 @@ SELECT
     jt.Name,
     c.CompanyName,
    ce.DateStart ,
+   lRate.NewRate,
    ce.DateEnd ,
     'Offboard' AS 'Group',
     cr.FirstName + ' ' + cr.LastName,
@@ -44,4 +49,7 @@ LEFT JOIN Customers c ON ce.CustomerId = c.Id
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) cr ON cr.UserId = ce.CreatedBy
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) m ON m.UserId = ce.LastModifiedBy
+OUTER APPLY (
+    SELECT TOP 1 * FROM RateIncreases WHERE EmployeeId = ce.EmployeeId ORDER BY EffectiveDate DESC
+    ) AS lRate
 WHERE c.Id != 1 AND ce.Id IS NOT NULL AND (c.CompanyName NOT LIKE 'codev%' AND c.CompanyName NOT LIKE '%breakthrough%')  AND ce.DateEnd IS NOT NULL AND c.Id != 281
