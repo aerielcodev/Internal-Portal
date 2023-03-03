740c8f76366952705c6776e8390d6a5fc70fc18d
sparkle-mermaid <sparkle.mermaid@live.com>
2023-03-03 14:18:10 -0700

    qualifiedCandidates

---
+++ QualifiedCandidates.sql
@@ -0,0 +1,21 @@
+SELECT     
+    cp.Id AS 'Id',     
+    cp.FirstName + ' ' + cp.LastName AS Name,     
+    s.Name AS Status,     
+CASE         
+    WHEN cp.GenderId  = 1 THEN 'Male'         
+    WHEN cp.GenderId = 2 THEN 'Female'         
+    ELSE 'Not Specified'     
+END AS Gender,     
+    cp.QualifiedDate AS 'Qualified Date',     
+    r.recruiter AS Recruiter,     
+    cp.Created,     
+    cp.CountryId  
+FROM CandidateProfileInformations cp 
+LEFT JOIN CandidateStatuses s ON s.Id = cp.CandidateStatusId 
+LEFT JOIN (     
+    SELECT         
+    Employees.Id AS eId,         
+    UserDetails.FirstName + ' ' + UserDetails.LastName AS recruiter     
+    FROM Employees     
+    INNER JOIN UserDetails ON UserDetails.UserId = Employees.UserId) AS r ON r.eId = cp.RecruiterId
