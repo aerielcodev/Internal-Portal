973c4a8d59bdb4d5d57f025459ff8ea8db8469a6
sparkle-mermaid <sparkle.mermaid@live.com>
2023-03-07 09:54:12 -0700

    excluded codev and dummy account

--- Job Opening Number History.sql
+++ Job Opening Number History.sql
@@ -4,17 +4,17 @@ SELECT
     c.CompanyName,
     ce.DateStart,
     jo.Name AS 'Job Opening Type'
-FROM JobOpeningNumbers jon
-INNER JOIN JobOpeningPositions jop ON jop.JobOpeningNumberId = jon.Id
+FROM INTERNALSERVICEDB.dbo.JobOpeningNumbers jon
+INNER JOIN INTERNALSERVICEDB.dbo.JobOpeningPositions jop ON jop.JobOpeningNumberId = jon.Id
 OUTER APPLY (
-    SELECT TOP 1 * FROM CustomerEmployees ce 
-		WHERE ce.JobOpeningPositionId = jop.Id
+    SELECT TOP 1 * FROM INTERNALSERVICEDB.dbo.CustomerEmployees ce 
+		WHERE ce.JobOpeningPositionId = jop.Id AND ce.IsDeleted = 0
 		ORDER BY DateStart ASC
     ) AS ce
 INNER JOIN (
     SELECT jt.Name,j.Id 
-    FROM JobOpenings j 
-INNER JOIN JobOpeningTypes jt ON jt.Id = j.JobOpeningTypeId) jo ON jo.Id = jop.JobOpeningId
+    FROM INTERNALSERVICEDB.dbo.JobOpenings j 
+INNER JOIN INTERNALSERVICEDB.dbo.JobOpeningTypes jt ON jt.Id = j.JobOpeningTypeId) jo ON jo.Id = jop.JobOpeningId
 INNER JOIN INTERNALSERVICEDB.dbo.Customers c ON c.Id = ce.CustomerId
-WHERE ce.DateStart IS NOT NULL
+WHERE c.Id != 1 AND c.Id != 281 AND ce.Id IS NOT NULL AND (c.CompanyName NOT LIKE 'codev%' AND c.CompanyName NOT LIKE '%breakthrough%') AND ce.DateStart IS NOT NULL
 ORDER BY ce.DateStart DESC
