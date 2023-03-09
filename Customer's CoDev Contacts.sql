1a00db8492887e4b391267fe6aa5d43c11579cb1
sparkle-mermaid <sparkle.mermaid@live.com>
2023-03-09 10:19:19 -0700

    added dateStart and dateEnd and EmployeeId

--- Customer's CoDev Contacts.sql
+++ Customer's CoDev Contacts.sql
@@ -1,12 +1,18 @@
 SELECT 
     cc.Id,
     cc.CustomerId,
+    emp.Id AS EmployeeId,
     emp.FirstName + ' ' + emp.LastName AS Name,
+    CASE
+    WHEN cc.DateStart = '0001-01-01 00:00:00.0000000 +00:00' THEN '2023-01-01 00:00:00.0000000 +00:00'
+    ELSE cc.DateStart
+    END AS DateStart,
+    cc.DateEnd,
     ct.Name AS Assignment
-FROM dbo.CustomerCodevContacts cc
+FROM INTERNALSERVICEDB.dbo.CustomerCodevContacts cc
 LEFT JOIN (
     SELECT emp.*,e.Id 
-    FROM Employees e 
-    INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = cc.EmployeeId
-LEFT JOIN dbo.CustomerCodevContactTypes ct ON ct.Id = cc.CustomerCodevContactTypeId
+    FROM INTERNALSERVICEDB.dbo.Employees e 
+    INNER JOIN INTERNALSERVICEDB.dbo.UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = cc.EmployeeId
+LEFT JOIN INTERNALSERVICEDB.dbo.CustomerCodevContactTypes ct ON ct.Id = cc.CustomerCodevContactTypeId
 WHERE cc.CustomerId != 281 /*281 is the dummy customer's id*/
