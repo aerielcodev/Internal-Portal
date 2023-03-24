16e8e8aa85af4452681599e6dfa39fbe7202eb35
sparkle-mermaid <sparkle.mermaid@live.com>
2023-03-24 09:30:01 -0600

    included row number

--- EmployeeQualifiedPositions.sql
+++ EmployeeQualifiedPositions.sql
@@ -4,9 +4,11 @@ SELECT
     upper(trim(emp.FirstName) + ' ' + trim(emp.LastName)) AS 'Team Member',
     jTeam.Name AS Team,
     jt.Name AS 'Type',
-    jp.Name AS Position
+    jp.Name AS Position,
+    row_number() over(partition by emp.CoDevId  ORDER BY emp.CoDevId ) AS RowNum
 FROM EmployeeJobPositions ep
 LEFT JOIN JobPositions jp ON ep.JobPositionId = jp.id
 LEFT JOIN JobTypes jt ON jt.id = jp.JobTypeId
 LEFT JOIN JobTeams jTeam ON jTeam.id = jt.JobTeamId
-LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ep.EmployeeId
+INNER JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ep.EmployeeId
+ORDER BY "Team Member" ASC
