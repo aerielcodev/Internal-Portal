46ac0bd686932ac1caf709112658b1c46dbd2bef
sparkle-mermaid <sparkle.mermaid@live.com>
2023-03-24 09:13:54 -0600

    updated to include createdby and modifiedby

--- CoDev Support (Corp) Placement History.sql
+++ CoDev Support (Corp) Placement History.sql
@@ -6,9 +6,13 @@ SELECT
     ce.JobTitle AS 'Job Title',
     c.CompanyName,
     CONVERT(date,ce.DateStart) AS Placement,
-    CONVERT(date,ce.DateEnd) AS 'Placement End'
+    CONVERT(date,ce.DateEnd) AS 'Placement End',
+    upper(trim(cb.FirstName) + ' ' + trim(cb.LastName)) AS CreatedBy,
+    upper(trim(cb.FirstName) + ' ' + trim(cb.LastName)) AS LastModifiedBy
 FROM Customers c 
-LEFT JOIN CustomerEmployees ce ON ce.CustomerId = c.Id
-LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
+INNER JOIN CustomerEmployees ce ON ce.CustomerId = c.Id
+INNER JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
+LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) cb ON cb.UserId = ce.CreatedBy
+LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) mb ON mb.UserId = ce.LastModifiedBy
 WHERE c.Id != 1 AND ce.Id IS NOT NULL AND (c.CompanyName LIKE 'codev%' OR c.CompanyName LIKE '%breakthrough%')
 ORDER BY [Team Member] ASC
