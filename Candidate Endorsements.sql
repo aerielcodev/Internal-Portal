31b7bf5bd7068243ee48fa76f011dc09d0c32790
sparkle-mermaid <sparkle.mermaid@live.com>
2023-05-01 12:03:28 -0600

    added createdby and lastmodifiedby

--- Candidate Endorsements.sql
+++ Candidate Endorsements.sql
@@ -17,7 +17,7 @@ SELECT
     END AS Recommendation,    
     naft.Name AS 'Not A Fit Category',
     jc.Note,
-    en.FirstName + ' ' + en.LastName AS 'Endorsed By',
+    eb.FirstName + ' ' + eb.LastName AS 'Endorsed By',
     jc.EndorsementStatusChangeDate AS 'Endorsement Status Change Date',
     d.FirstName + ' ' + d.LastName AS 'Declined By',
     IIF(jc.InterviewRequestedBy IS NOT NULL,'Y','N') AS 'Interview Requested',
@@ -33,15 +33,26 @@ SELECT
 FROM JobOpeningNumbers jon 
 INNER JOIN JobOpeningPositions jop ON jop.JobOpeningNumberId = jon.Id
 INNER JOIN JobOpenings j ON j.Id = jop.JobOpeningId
-INNER JOIN JobOpeningCandidates jc ON jc.JobOpeningId  = j.Id 
-LEFT JOIN CandidateProfileInformations c ON c.Id = jc.CandidateId AND (c.FirstName NOT LIKE '%demo%'  OR  c.LastName NOT LIKE '%demo%' )
-LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) e ON e.Id = jc.EmployeeId AND (e.FirstName NOT LIKE '%demo%'  OR  e.LastName NOT LIKE '%demo%' )
+INNER JOIN JobOpeningCandidates jc ON jc.JobOpeningId  = j.Id
+LEFT JOIN CandidateProfileInformations c ON c.Id = jc.CandidateId 
+AND (c.FirstName NOT LIKE '%demo%'  OR  c.LastName NOT LIKE '%demo%' )
+LEFT JOIN (
+    SELECT 
+    emp.FirstName,
+    emp.LastName,
+    emp.CodevId,
+    e.Id 
+    FROM Employees e 
+    INNER JOIN UserDetails emp ON emp.UserId = e.UserId)
+     e ON e.Id = jc.EmployeeId 
+     AND (e.FirstName NOT LIKE '%demo%'  OR  e.LastName NOT LIKE '%demo%' )
 LEFT JOIN JobOpeningEndorsementStatuses js ON js.Id = jc.JobOpeningEndorsementStatusId
 LEFT JOIN NotAFitCategoryTypes naft ON naft.Id = jc.NotAFitCategoryTypeId
-LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) en ON en.UserId = jc.CreatedBy /*user who endorsed the candidate*/
+LEFT JOIN UserDetails eb ON eb.UserId = jc.CreatedBy /*user who endorsed the candidate*/ 
 LEFT JOIN CustomerUsers d ON d.UserId = jc.DeclinedBy
 LEFT JOIN Customers cx ON cx.Id = j.CustomerId
 /*remove any dummy candidate endorsements and any endorsements coming for codev/breakthrough*/
- WHERE j.CustomerId != 281 AND (cx.CompanyName NOT LIKE 'codev%' AND cx.CompanyName NOT LIKE '%breakthrough%' ) 
+ WHERE j.CustomerId != 281 AND 
+ (cx.CompanyName NOT LIKE 'codev%' AND cx.CompanyName NOT LIKE '%breakthrough%' ) 
   AND jc.Created >= CONVERT(DATE,'2022-12-06') /*Dec. 6, 2022 is the official day that we started endorsing candidates to customers*/
 
