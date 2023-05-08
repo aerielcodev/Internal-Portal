d485c3747cb8141cdee1aebdff9c2fd9befcb5aa
sparkle-mermaid <sparkle.mermaid@live.com>
2023-05-08 11:45:25 -0600

    included qualified positions

--- CoDev Bench History.sql
+++ CoDev Bench History.sql
@@ -7,6 +7,9 @@ SELECT
     c.CompanyName,
     ce.DateStart,
     ce.DateEnd,
+    ePosn.teams AS 'Qualified Teams',
+    ePosn.types AS 'Qualified Types',
+    ePosn.posn AS 'Qualified Positions',
     cb.FirstName + ' ' + cb.LastName AS 'Created By',
     m.FirstName + ' ' + m.LastName AS 'Modified By'
 FROM Customers c 
@@ -14,6 +17,18 @@ LEFT JOIN CustomerEmployees ce ON ce.CustomerId = c.Id
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) m ON m.UserId = ce.LastModifiedBy
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) cb ON cb.UserId = ce.CreatedBy
+LEFT JOIN (
+    SELECT
+        EmployeeId,
+        string_agg(jp.Name,', ') AS posn,
+        string_agg(jt.Name,', ') AS types,
+        string_agg(jteam.Name,', ') AS teams
+  FROM EmployeeJobPositions ep
+  INNER JOIN JobPositions jp ON jp.Id = ep.JobPositionId
+  INNER JOIN JobTypes jt ON jt.Id = jp.JobTypeId
+  INNER JOIN JobTeams jteam ON jteam.Id = jt.JobTeamId
+  GROUP BY EmployeeId
+) AS ePosn ON ePosn.EmployeeId = ce.EmployeeId
 WHERE c.Id = 1 AND ce.IsDeleted = 0
 AND (emp.FirstName NOT LIKE '%demo%'  AND  emp.FirstName NOT LIKE '%demo%')
 ORDER BY [Team Member] ASC
