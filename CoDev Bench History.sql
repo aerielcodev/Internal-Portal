c828f687dea0989ccafb343458041cfcd82e6da0
Aeriel Soriano <27873562+aerielsoriano@users.noreply.github.com>
2023-06-02 07:27:58 -0600

    updated query

--- CoDev Bench History.sql
+++ CoDev Bench History.sql
@@ -6,7 +6,7 @@ SELECT
     ce.jobTitle AS 'Job Title',
     c.CompanyName,
     ce.DateStart,
-    ce.DateEnd,
+    coalesce(ce.DateEnd, e.DeletedDate) AS DateEnd,
     coalesce(lRate.NewRate,lRate2.NewRate) AS 'Latest Rate',
     ePosn.teams AS 'Qualified Teams',
     ePosn.types AS 'Qualified Types',
@@ -18,6 +18,7 @@ LEFT JOIN CustomerEmployees ce ON ce.CustomerId = c.Id
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = ce.EmployeeId
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) m ON m.UserId = ce.LastModifiedBy
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) cb ON cb.UserId = ce.CreatedBy
+LEFT JOIN Employees e ON e.Id = ce.EmployeeId AND e.Status = 3
 LEFT JOIN (
     SELECT
         EmployeeId,
--- Customer's CoDev Contacts.sql
+++ Customer's CoDev Contacts.sql
@@ -1,15 +1,31 @@
-SELECT 
-    cc.Id,
-    cc.CustomerId,
-    emp.Id AS EmployeeId,
-    emp.FirstName + ' ' + emp.LastName AS Name,
-    cast(cc.DateStart AS Date) AS DateStart,
-    cast(cc.DateEnd AS Date) AS DateEnd,
-    ct.Name AS Assignment
-FROM INTERNALSERVICEDB.dbo.CustomerCodevContacts cc
-LEFT JOIN (
-    SELECT emp.*,e.Id 
-    FROM INTERNALSERVICEDB.dbo.Employees e 
-    INNER JOIN INTERNALSERVICEDB.dbo.UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = cc.EmployeeId
-LEFT JOIN INTERNALSERVICEDB.dbo.CustomerCodevContactTypes ct ON ct.Id = cc.CustomerCodevContactTypeId
-WHERE cc.CustomerId != 281 /*281 is the dummy customer's id*/
+WITH cte AS (SELECT DISTINCT
+        cc.Id,
+        cc.CustomerId,
+        emp.Id AS EmployeeId,
+        concat(emp.FirstName,' ',emp.LastName) AS Name,
+        cast(cc.DateStart AS Date) AS DateStart,
+        IIF(cc.DateEnd IS NULL AND c.Status = 2,CAST(lastP.endOfLastPlacement AS Date),cast(cc.DateEnd AS Date)) AS DateEnd,
+        ct.Name AS Assignment,
+        iif(cast(cc.DateStart AS Date) = '0001-01-01' AND cc.DateEnd IS NULL AND cc.IsActive = 0,'Y','N') AS isInvalid
+    FROM INTERNALSERVICEDB.dbo.CustomerCodevContacts cc
+    LEFT JOIN Customers c ON c.Id = cc.CustomerId
+    LEFT JOIN (
+        SELECT 
+        emp.FirstName,
+        emp.LastName,
+        e.Id 
+        FROM INTERNALSERVICEDB.dbo.Employees e 
+        INNER JOIN INTERNALSERVICEDB.dbo.UserDetails emp ON emp.UserId = e.UserId) emp ON emp.Id = cc.EmployeeId
+    LEFT JOIN INTERNALSERVICEDB.dbo.CustomerCodevContactTypes ct ON ct.Id = cc.CustomerCodevContactTypeId
+    LEFT JOIN (
+        SELECT
+            max(DateEnd) AS endOfLastPlacement,
+            CustomerId
+        FROM CustomerEmployees
+        GROUP BY CustomerId
+    ) AS lastP ON lastP.CustomerId = cc.CustomerId
+    WHERE cc.CustomerId != 281 /*281 is the dummy customer's id*/)
+SELECT
+    *
+FROM cte
+WHERE cte.isInvalid = 'N'
