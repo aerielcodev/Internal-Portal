76efbd18fb87a35bb3fa9ac3a717de282e8ca5a9
Aeriel Soriano <27873562+aerielsoriano@users.noreply.github.com>
2023-07-26 11:10:34 -0600

    added row numbers

--- Scheduled Interviews.sql
+++ Scheduled Interviews.sql
@@ -13,7 +13,11 @@ SELECT
     i.Created AS Created,
     ud.FirstName + ' ' + ud.LastName AS 'Created By',
     jc.JobOpeningId AS JobOpeningId,
-    IIF(jc.InterviewRequestedBy IS NOT NULL, 'Y','N') AS 'Interview Requested'
+    i.LastModified,
+    mb.FirstName + ' ' + mb.LastName AS 'Last Modified By',
+    IIF(jc.InterviewRequestedBy IS NOT NULL, 'Y','N') AS 'Interview Requested',
+    ROW_NUMBER() OVER(PARTITION BY jc.Id ORDER BY i.ScheduleDate DESC) AS rn
+
 FROM JobOpeningCandidateScheduleInterviews i 
 INNER JOIN JobOpeningCandidates jc ON jc.Id = i.JobOpeningCandidateId
 INNER JOIN JobOpenings j ON j.Id = jc.JobOpeningId
@@ -27,6 +31,8 @@ LEFT JOIN (
     INNER JOIN UserDetails emp ON emp.UserId = e.UserId
     ) e ON e.Id = jc.EmployeeId
 LEFT JOIN UserDetails AS ud ON ud.UserId = i.CreatedBy
+LEFT JOIN UserDetails AS mb ON mb.UserId = i.LastModifiedBy
+
 WHERE j.CustomerId != 281 
 AND (cx.CompanyName NOT LIKE 'codev%' 
 AND cx.CompanyName NOT LIKE '%breakthrough%' )/**281 is the dummy customer*/ 
