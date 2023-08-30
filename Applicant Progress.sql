35423761d8c1a594e83b16730bbc6b5e012d18b3
Aeriel Soriano <27873562+aerielsoriano@users.noreply.github.com>
2023-08-30 20:28:17 +0000

    removed Recruiter for now and added their Applied Date

---
+++ Applicant Progress.sql
@@ -0,0 +1,38 @@
+/*displays various applicant touchpoints*/
+SELECT
+    a.Id AS ApplicantId,
+    ap.Id AS ApplicantJobPostingsId,
+    cp.Id AS CandidateId,
+    CONCAT(TRIM(a.FirstName),' ',TRIM(a.LastName)) AS Applicant,
+    jp.Title AS 'Associated Job Posting',
+    jp.Number,
+    iif(cb.UserId IS NULL,a.CreatedBy,CONCAT(cb.FirstName,' ',cb.LastName)) AS CreatedBy,
+    cp.QualifiedDate,
+    ast.Name AS 'Status',
+    ap.DateApplied AS 'Date Applied',
+    ajp.Created AS 'Status Updated On',
+    IIF(ub.FirstName IS NULL,ajp.CreatedBy,CONCAT(ub.FirstName,' ',ub.LastName)) AS 'Status Updated By'
+FROM Applicants a
+LEFT JOIN UserDetails cb ON cb.UserId = a.CreatedBy
+INNER JOIN ApplicantJobPostings ap ON ap.ApplicantId = a.Id
+LEFT JOIN JobPostings jp ON jp.Id = ap.JobPostingId
+LEFT JOIN CandidateProfileInformations cp ON cp.Id = ap.CandidateId
+LEFT JOIN ApplicantJobPostingProgresses ajp ON ajp.Id = ap.ProgressId
+LEFT JOIN ApplicationStages ast ON ast.Id = ajp.ApplicationStageId
+/*LEFT JOIN (     
+    SELECT         
+    Employees.Id AS eId,         
+    UserDetails.FirstName + ' ' + UserDetails.LastName AS recruiter     
+    FROM Employees     
+    INNER JOIN UserDetails ON UserDetails.UserId = Employees.UserId) AS r ON r.eId = cp.RecruiterId*/
+    LEFT JOIN UserDetails ub ON ub.UserId = ajp.CreatedBy
+WHERE  
+    a.Created >= CONVERT(DATE,'2023-05-17') /*date v2 careers was launched*/
+AND
+(
+        LOWER(a.FirstName) NOT LIKE '%test%'
+        AND LOWER(a.LastName) NOT LIKE '%test%'
+        AND LOWER(a.FirstName) NOT LIKE '%demo%'
+        AND LOWER(a.LastName) NOT LIKE '%demo%'
+    ) 
+ORDER BY ajp.Created DESC
