4bb468b76cb860d20b9d4dbd9fb25a168a40a742
Aeriel Soriano <27873562+aerielsoriano@users.noreply.github.com>
2023-08-30 14:06:46 +0000

    added new categories

--- Offboarding Reasons.sql
+++ Offboarding Reasons.sql
@@ -2,16 +2,19 @@ SELECT
     o.Id,
     o.CustomerEmployeeId,
     r.Cat AS 'Offboarding Category',
-    r.SubCat AS 'SubCategory', 
+    r.PrimCat AS 'Subcategory',
+    r.SubCat AS 'Secondary SubCategory', 
     o.Note
 FROM EmployeeOffboardings o
 INNER JOIN (
     SELECT 
         s.EmployeeOffboardingId,
         oc.Name AS Cat,
+        s3.Name AS PrimCat,
         s2.Name AS SubCat
-    FROM EmployeeOffboardingSubCategories s 
-    INNER JOIN OffboardingSubCategories s2 ON s.OffboardingSubCategoryId = s2.Id
-    INNER JOIN OffboardingCategories oc ON oc.Id = s2.OffboardingCategoryId
+    FROM EmployeeOffboardingSecondarySubCategories s 
+    INNER JOIN OffboardingSecondarySubCategories s2 ON s2.Id = s.OffboardingSecondarySubCategoryId
+    INNER JOIN OffboardingSubCategories s3 ON s3.Id = s2.SubCategoryId
+    INNER JOIN OffboardingCategories oc ON oc.Id = s3.OffboardingCategoryId
     ) AS r ON r.EmployeeOffboardingId = o.Id
     ORDER BY o.CustomerEmployeeId ASC
