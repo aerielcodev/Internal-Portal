ab36fd5e8bf1a010262d8e306266cdb1113857b1
Aeriel Soriano <27873562+aerielsoriano@users.noreply.github.com>
2023-09-07 11:48:07 -0600

    concatenated name and added filter if team member is active or not

--- Employee PTOs.sql
+++ Employee PTOs.sql
@@ -4,13 +4,13 @@ pto.EmployeeId,
 s.Name AS 'Status', 
 ud.Email AS Email, 
 ud.CodevId AS 'Employee Number', 
-ud.FirstName AS 'First Name', 
-ud.LastName AS 'Last Name' , 
+CONCAT(ud.FirstName,' ',ud.LastName) AS 'Team Member',
+ec.Name AS 'Member Category',
 pto.Created AS 'PTO File Date', 
 ft.Name AS 'Filing Type', 
 lt.Name AS 'Leave Type', 
-pto.DateFrom AS 'Date From', 
-pto.DateTo AS 'Date To', 
+CAST(pto.DateFrom AS date) AS 'Date From', 
+CAST(pto.DateTo AS date) AS 'Date To', 
 pto.LeaveCount AS 'Leave Qty', 
 pto.ApprovalDate AS 'Approval Date' ,
 CASE
@@ -18,7 +18,9 @@ WHEN cu.UserId IS NULL
 THEN app.FirstName + ' ' + app.LastName
 ELSE cu.FirstName + ' ' + cu.LastName
 END AS Approver,
-cu.UserId AS 'Customer UserId'
+cu.UserId AS 'Customer UserId',
+iif(e.DeletedDate IS NULL,'Y','N') AS isActive
+
 FROM [dbo].[Ptos] pto 
 LEFT JOIN [dbo].[FilingTypes] ft ON ft.Id = pto.FilingTypeId 
 LEFT JOIN [dbo].[PtoStatuses] s ON s.Id = pto.StatusId 
@@ -27,4 +29,5 @@ INNER JOIN Employees e ON e.Id = pto.EmployeeId
 INNER JOIN UserDetails ud ON ud.UserId = e.UserId 
 LEFT JOIN UserDetails app ON app.UserId = pto.ApprovalBy
 LEFT JOIN CustomerUserDetails cu ON cu.UserId = pto.ApprovalBy
+LEFT JOIN EmployeeCategories ec ON ec.Id = e.CategoryId
 ORDER BY Id DESC
