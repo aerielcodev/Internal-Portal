b09b9536a2f752746e312d00716934cab397b7e4
Aeriel Soriano <27873562+aerielsoriano@users.noreply.github.com>
2023-09-08 08:28:48 -0600

    added filter to remove test and codev accounts

--- Customer Users.sql
+++ Customer Users.sql
@@ -13,3 +13,7 @@ SELECT DISTINCT
   LEFT JOIN Customers c ON c.Id = u.CustomerId
   LEFT JOIN CustomerUserRoles cr ON cr.Id = u.CustomerUserRoleId
   WHERE (ud.FirstName + ' ' + ud.LastName IS NOT NULL) AND (ud.FirstName + ' ' + ud.LastName != ' ') AND u.CustomerId != 281
+  AND (c.CompanyName NOT LIKE 'codev%' 
+  AND c.CompanyName NOT LIKE '%breakthrough%' 
+  AND c.CompanyName NOT LIKE '%Test%')
+  AND ud.Email NOT LIKE '%@codev.com'
