af5f27525b8ac9a94bf56cbc164fd1c35bdc6ced
Aeriel Soriano <27873562+aerielsoriano@users.noreply.github.com>
2023-09-11 14:53:06 -0600

    added customerId and removed any codev company accts

--- Customer Portal Login History.sql
+++ Customer Portal Login History.sql
@@ -5,6 +5,7 @@ SELECT DISTINCT
       l.[DateTime] AS 'UTC DateTime',
       ud.FirstName + ' ' + ud.LastName AS Name,
       c.CompanyName AS Customer,
+      c.Id AS customerId,
       cr.Name AS 'Role Name',
       u.IsActive
   FROM [dbo].[CustomerPortalLogins] l
@@ -12,4 +13,9 @@ SELECT DISTINCT
   INNER JOIN CustomerUsers u ON u.CustomerUserDetailsId = ud.Id
   LEFT JOIN Customers c ON c.Id = u.CustomerId
   LEFT JOIN CustomerUserRoles cr ON cr.Id = u.CustomerUserRoleId
+  WHERE ud.Status = 1
+  AND c.Id != 281 AND 
+  (c.CompanyName NOT LIKE 'codev%' 
+  AND c.CompanyName NOT LIKE '%breakthrough%' 
+  AND c.CompanyName NOT LIKE '%Test%')/**281 is the dummy customer*/ 
   ORDER BY l.[DateTime] DESC
