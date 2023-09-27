734acf3c9e5aa09b090d4e28bef0380d169af728
Aeriel Soriano <27873562+aerielsoriano@users.noreply.github.com>
2023-09-27 13:31:41 -0600

    added source of candidate

--- QualifiedApplicants.sql
+++ QualifiedApplicants.sql
@@ -9,7 +9,24 @@ SELECT
     r.recruiter AS Recruiter,     
     jp.Title AS 'Associated Job Posting',
     jp.Number,
-    DATEDIFF(day,ap.DateApplied,cp.QualifiedDate) AS 'Days from Application to Qualified'
+    DATEDIFF(day,ap.DateApplied,cp.QualifiedDate) AS 'Days from Application to Qualified',
+        CASE
+        WHEN ap.SourceId = 1 THEN 'CoDev Career Site'
+        WHEN ap.SourceId = 2 THEN 'Facebook'
+        WHEN ap.SourceId = 3 THEN 'Google'
+        WHEN ap.SourceId = 4 THEN 'Indeed'
+        WHEN ap.SourceId = 5 THEN 'Job Fair'
+        WHEN ap.SourceId = 6 THEN 'JobStreet'
+        WHEN ap.SourceId = 7 THEN 'LinkedIn'
+        WHEN ap.SourceId = 8 THEN 'Mynimo'
+        WHEN ap.SourceId = 9 THEN 'Naukri'
+        WHEN ap.SourceId = 10 THEN 'Organizational Partnership'
+        WHEN ap.SourceId = 11 THEN 'Applicant Referral'
+        WHEN ap.SourceId = 12 THEN 'Employee Referral'
+        WHEN ap.SourceId = 13 THEN 'External Referral'
+        WHEN ap.SourceId = 14 THEN 'Word of Mouth'
+        WHEN ap.SourceId = 15 THEN 'Other'
+    END AS 'Source of Candidate'
 FROM Applicants a
 LEFT JOIN UserDetails cb ON cb.UserId = a.CreatedBy
 LEFT JOIN ApplicantJobPostings ap ON ap.ApplicantId = a.Id
@@ -29,5 +46,5 @@ AND
         AND LOWER(a.LastName) NOT LIKE '%test%'
         AND LOWER(a.FirstName) NOT LIKE '%demo%'
         AND LOWER(a.LastName) NOT LIKE '%demo%'
-    ) 
+    ) AND ap.StatusId <> 3 /*StatusId = 3 are deleted profiles*/
 ORDER BY a.Created DESC
