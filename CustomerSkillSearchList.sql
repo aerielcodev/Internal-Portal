f397e169094b18b6d021525457fab52843cbe4a6
Aeriel Soriano <27873562+aerielsoriano@users.noreply.github.com>
2023-10-05 19:39:59 -0600

    lists all the skills a customer has searched in the customer portal

---
+++ CustomerSkillSearchList.sql
@@ -0,0 +1,21 @@
+WITH SplitSkills AS (
+  SELECT 
+    Id,
+    CAST(NULLIF(LEFT(SUBSTRING(SkillsSearch, 2, LEN(SkillsSearch) - 2), CHARINDEX(',', SUBSTRING(SkillsSearch, 2, LEN(SkillsSearch) - 2) + ',') - 1), '') AS INT) AS skill,
+    SUBSTRING(SUBSTRING(SkillsSearch, 2, LEN(SkillsSearch) - 2), CHARINDEX(',', SUBSTRING(SkillsSearch, 2, LEN(SkillsSearch) - 2) + ',') + 1, LEN(SUBSTRING(SkillsSearch, 2, LEN(SkillsSearch) - 2))) AS remaining_skill
+  FROM DiscoverAuditTrails
+  WHERE SkillsSearch NOT LIKE '%null%'
+  UNION ALL
+  SELECT 
+    Id,
+    CAST(NULLIF(LEFT(remaining_skill, CHARINDEX(',', remaining_skill + ',') - 1), '') AS INT) AS skill,
+    SUBSTRING(remaining_skill, CHARINDEX(',', remaining_skill + ',') + 1, LEN(remaining_skill))
+  FROM SplitSkills
+  WHERE remaining_skill != ''
+)
+SELECT 
+    ss.Id, 
+    s.Name
+FROM SplitSkills ss
+JOIN Skills s ON s.Id = ss.skill
+
