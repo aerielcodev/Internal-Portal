0e70a164d05e9a58379942fa4b3ef3e10dd5890f
Aeriel Soriano <27873562+aerielsoriano@users.noreply.github.com>
2023-10-25 11:44:05 -0600

    identifier if team member is customer-facing or not

--- O3Logs.sql
+++ O3Logs.sql
@@ -7,7 +7,8 @@ SELECT
     e2.FirstName +' ' + e2.LastName AS 'Team Member', 
     s.Email AS 'Talent Supervisor email', 
     e2.Email AS 'Team Member email' ,
-    e2.CodevId AS 'Team Member CoDev Id'
+    e2.CodevId AS 'Team Member CoDev Id',
+    iif(ce.Id IS NOT NULL,'Y','N') AS 'Customer-facing Team Member'
 FROM [dbo].[EmployeeFeedbacks] AS f 
 LEFT JOIN [dbo].[FeedbackTypes] AS ft ON ft.[Id] = f.[FeedbackTypeId] 
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) 
@@ -15,4 +16,8 @@ LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.
 /*LEFT JOIN [dbo].[UserDetailsView] s ON f.CreatedBy = s.UserId */
 LEFT JOIN [dbo].[Employees] e ON f.EmployeeId = e.Id 
 LEFT JOIN [dbo].[UserDetails] e2 ON e.UserId = e2.UserId 
+LEFT JOIN CustomerEmployees ce ON ce.EmployeeId = f.EmployeeId 
+    AND (f.O3FeedbackDate >= ce.DateStart AND (f.O3FeedbackDate <= ce.DateEnd OR ce.DateEnd IS NULL)) 
+    AND ce.IsDeleted = 0 
+    AND ce.CustomerId NOT IN (1,281) AND ce.JobOpeningPositionId IS NOT NULL
 WHERE f.FeedbackTypeId = 3 
