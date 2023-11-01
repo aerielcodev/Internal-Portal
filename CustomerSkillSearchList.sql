7fdfaea4463ffc6500875bc05f9ab5b83ea5cff4
Aeriel Soriano <27873562+aerielsoriano@users.noreply.github.com>
2023-11-01 06:43:26 -0600

    updated to work with cte better

--- CustomerSkillSearchList.sql
+++ CustomerSkillSearchList.sql
@@ -1,22 +1,30 @@
-/*List all skills searched in the Customer Portal*/
 WITH SplitSkills AS (
-  SELECT 
-    Id,
-    CAST(NULLIF(LEFT(SUBSTRING(SkillsSearch, 2, LEN(SkillsSearch) - 2), CHARINDEX(',', SUBSTRING(SkillsSearch, 2, LEN(SkillsSearch) - 2) + ',') - 1), '') AS INT) AS skill,
-    SUBSTRING(SUBSTRING(SkillsSearch, 2, LEN(SkillsSearch) - 2), CHARINDEX(',', SUBSTRING(SkillsSearch, 2, LEN(SkillsSearch) - 2) + ',') + 1, LEN(SUBSTRING(SkillsSearch, 2, LEN(SkillsSearch) - 2))) AS remaining_skill
-  FROM DiscoverAuditTrails
-  WHERE SkillsSearch NOT LIKE '%null%'
-  UNION ALL
-  SELECT 
-    Id,
-    CAST(NULLIF(LEFT(remaining_skill, CHARINDEX(',', remaining_skill + ',') - 1), '') AS INT) AS skill,
-    SUBSTRING(remaining_skill, CHARINDEX(',', remaining_skill + ',') + 1, LEN(remaining_skill))
-  FROM SplitSkills
-  WHERE remaining_skill != ''
+    SELECT
+        Id,
+        CONVERT(NVARCHAR(MAX), NULL) AS Skill,
+        SUBSTRING(SkillsSearch, 2, LEN(SkillsSearch) - 2) AS RemainingSkills
+    FROM DiscoverAuditTrails
+    WHERE SkillsSearch NOT LIKE '%null%'
+    UNION ALL
+    SELECT
+        Id,
+        CASE
+            WHEN CHARINDEX(',', RemainingSkills) > 0
+                THEN LEFT(RemainingSkills, CHARINDEX(',', RemainingSkills) - 1)
+            ELSE RemainingSkills
+        END AS Skill,
+        CASE
+            WHEN CHARINDEX(',', RemainingSkills) > 0
+                THEN SUBSTRING(RemainingSkills, CHARINDEX(',', RemainingSkills) + 1, LEN(RemainingSkills))
+            ELSE ''
+        END AS RemainingSkills
+    FROM SplitSkills
+    WHERE RemainingSkills != ''
 )
-SELECT 
-    ss.Id, 
+SELECT
+    ss.Id,
     s.Name AS Skill
 FROM SplitSkills ss
 JOIN Skills s ON s.Id = ss.skill
-
+WHERE ss.Skill != ''
+OPTION (MAXRECURSION 0);
