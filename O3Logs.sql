f404f7dbe924f7f9a7ab77b51f635b084e07f6cc
Aeriel Soriano <27873562+aerielsoriano@users.noreply.github.com>
2023-12-19 18:10:05 -0700

    updated query

--- O3Logs.sql
+++ O3Logs.sql
@@ -3,14 +3,14 @@ SELECT
     f.O3FeedbackDate AS 'O3 Date', 
     ft.Name AS 'Feedback Type', 
     s.Id AS SupervisorEmployeeId,
-    s.FirstName + ' ' + s.LastName AS 'Talent Supervisor', 
-    e2.FirstName +' ' + e2.LastName AS 'Team Member', 
+    CONCAT(s.FirstName , ' ' , s.LastName) AS 'Talent Supervisor', 
+    CONCAT(e2.FirstName ,' ' , e2.LastName) AS 'Team Member', 
     s.Email AS 'Talent Supervisor email', 
     e2.Email AS 'Team Member email' ,
     e2.CodevId AS 'Team Member CoDev Id',
     iif(ce.Id IS NOT NULL,'Y','N') AS 'Customer-facing Team Member'
 FROM [dbo].[EmployeeFeedbacks] AS f 
-LEFT JOIN [dbo].[FeedbackTypes] AS ft ON ft.[Id] = f.[FeedbackTypeId] 
+JOIN [dbo].[FeedbackTypes] AS ft ON ft.[Id] = f.[FeedbackTypeId] 
 LEFT JOIN (SELECT emp.*,e.Id FROM Employees e INNER JOIN UserDetails emp ON emp.UserId = e.UserId) 
     s ON s.UserId = f.CreatedBy
 /*LEFT JOIN [dbo].[UserDetailsView] s ON f.CreatedBy = s.UserId */
@@ -20,4 +20,5 @@ LEFT JOIN CustomerEmployees ce ON ce.EmployeeId = f.EmployeeId
     AND (f.O3FeedbackDate >= ce.DateStart AND (f.O3FeedbackDate <= ce.DateEnd OR ce.DateEnd IS NULL)) 
     AND ce.IsDeleted = 0 
     AND ce.CustomerId NOT IN (1,281) AND ce.JobOpeningPositionId IS NOT NULL
-WHERE f.FeedbackTypeId = 3 
+WHERE f.FeedbackTypeId = 3 AND f.O3FeedbackDate IS NOT NULL AND f.IsDraft = 0
+ORDER BY [O3 Date] DESC
